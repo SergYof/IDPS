922df04b775963bdba65daa302ea7ef663fc223c
shapilev <lev.shapiro@gamago.net>
2025-12-15 16:44:40 +0200

    fix main.py

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from .cracks.Port_Scanning.portscan import PortScanCrack
+from cracks.base import Crack
+from cracks.Port_Scanning.portscan import PortScanCrack
+from cracks.ARP_Spoofing.arpspoof import ARPSpoofCrack
+from cracks.dns.dnsspoof import DNSSpoofCrack
+from cracks.MITM.mitm import MITMCrack
+from time import sleep
 
-portscanning = PortScanCrack()
+ATTACKS: list[type[Crack]] = [PortScanCrack, ARPSpoofCrack, DNSSpoofCrack, MITMCrack]
+CHECKS_INTERVAL = 5
 
-portscanning.defend("94.230.80.78")
+def cycleCracks():
+    for attackClass in ATTACKS:
+        attack = attackClass()
+        attack.identify()
+
+
+def main() -> None:
+    while True:
+        cycleCracks()
+        sleep(CHECKS_INTERVAL)
+    
+
+
+if __name__ == "__main__":
+    main()
