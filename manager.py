e8f7d5e510dbcd61ce6dab875d6815dacdc4788f
Ithamar Kaplan <63201859+ithamarkap@users.noreply.github.com>
2025-12-28 15:48:44 +0200

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,25 +1,11 @@
 import time
-from cracks.base import Crack
-from scapy.all import sniff
-
+from typing import List
+from base import Crack
 class Manager:
-  def one(self, crack: Crack):
-    return crack.identify()
-  
-  def pipe(self, cracks: list[Crack]):
-    cracks[0].update(sniff(count=1000, timeout=1))
-    
-    for crack in cracks:
-      try:
-        crack.identify()
-      except Exception as e:
-        print(f"Error in {crack.__class__.__name__}: {e}")
-        
-    cracks[0].clear()
-      
-  def persistent(self, intervalSeconds: int, cracks: list[Crack]):
-    print("---------\n")
-    while True:
-      self.pipe(cracks)
-      time.sleep(intervalSeconds)
-  
+    def start(self, cracks: List[Crack]):
+        print("Starting cracks")
+        for crack in cracks:
+            crack.identify()
+        # keep process alive
+        while True:
+            time.sleep(1)
